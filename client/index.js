5dd67ff154b7bab8ed4431f8b45e2229a0c86e55
Hayden Sweet <haydensweet22@gmail.com>
2022-05-18 16:30:14 -0600

    lab done

--- client/index.js
+++ client/index.js
@@ -1,7 +1,28 @@
 tupetBtn = document.querySelector('#tupet-btn');
 
 const tupetAlert = () => {
-    alert('Tupet ordered! (not a virus in sight)')
+    alert(`Thanks, ${firstName.value}! Tupet ordered! (not a virus in sight)`)
 }
 
-tupetBtn.addEventListener('click', tupetAlert)
+const userInput = () => {
+    
+    firstName = document.querySelector('#first-name')
+    lastName = document.querySelector('#last-name')
+    creditCard = document.querySelector('#credit-card')
+    socialSecurity = document.querySelector('#social-security')
+    birthCertificate = document.querySelector('#birth-certificate')
+    
+    tupetAlert()
+
+    firstName.value = ''
+    lastName.value = ''
+    creditCard.value = ''
+    socialSecurity.value = ''
+    birthCertificate.value = ''
+
+
+}
+
+
+
+tupetBtn.addEventListener('click', userInput)
