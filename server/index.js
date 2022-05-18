a662e725497e48204279e05dabfe56dd6aa41538
Hayden Sweet <haydensweet22@gmail.com>
2022-05-18 15:09:04 -0600

    endpoint

--- server/index.js
+++ server/index.js
@@ -3,8 +3,13 @@ const path = require('path');
 
 const app = express();
 
+// Middleware
 app.use(express.static(path.join(__dirname, '../client')))
 
+// Endpoints
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.js'))
+  })
 
 const port = process.env.PORT || 4444;
 
